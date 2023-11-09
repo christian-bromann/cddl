79c574d3bce1e3280337858cb65e5850ff0d25ee
Christian Bromann <git@bromann.dev>
2023-11-09 11:35:52 -0800

    Fix CDDL issues within the WebDriver Bidi spec
    reported in:
    - https://github.com/w3c/webdriver-bidi/pull/600
    - https://github.com/w3c/webdriver-bidi/pull/599

--- src/cli/commands/validate.ts
+++ src/cli/commands/validate.ts
@@ -30,6 +30,7 @@ export const handler = (argv: ArgumentsCamelCase<ValidateArguments>) => {
     try {
         parse(filePath)
 
+
         /**
          * ToDo check for
          *  - missing group declarations
